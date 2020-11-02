f2fc63dc44f5e0aede7aa29a7face8a9087aca48
vidhibhati16 <71395900+vidhibhati16@users.noreply.github.com>
2020-11-02 16:20:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,18 +5,24 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var engine, world;
 var paper1;
+//create a variable for the dustbin
 var ground1;
 
 
 
 function setup() {
 	var canvas = createCanvas(800, 700);
+	//rectMode(CENTER);
+	
 	engine = Engine.create();
 	world = engine.world;
 
 	ground1 = new ground(600, height, 1200, 20);
 	paper1 = new paper(100, 650);
 
+	//create a dustbin
+	
+	//Engine.run(engine);
 
 	
 }
@@ -29,9 +35,12 @@ function draw() {
   
   ground1.display();
   paper1.display();
+	
+//display the dustbin object
   
  
 }
 
+//make a function keyPressed() [see hint no. 4]
 
 
